4be791b09577c621199d38477da8fef130f58cc1
Andrew Usher <andrewusher00@gmail.com>
2019-11-14 01:26:18 -0600

    feat: add babel optional chaining plugin

--- .babelrc.js
+++ .babelrc.js
@@ -1,11 +1,15 @@
 module.exports = api => {
   api.cache(true)
   return {
-    plugins: ['@babel/plugin-proposal-class-properties'],
-    presets: [['@babel/env', {
-      targets: {
-        node: 'current'
-      }
-    }]]
+    plugins: [
+      '@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-optional-chaining'
+    ],
+    presets: [
+      ['@babel/env', {
+        targets: {
+          node: 'current'
+        }
+      }],
+    ]
   }
 }
